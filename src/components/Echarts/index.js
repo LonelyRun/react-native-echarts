411a4ae1099c424a2d7f9e51437e6d56249eec1c
vaccer <852649170@qq.com>
2020-08-06 15:37:34 +0800

    Update index.js

--- src/components/Echarts/index.js
+++ src/components/Echarts/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { WebView, View, StyleSheet, Platform } from 'react-native';
+import { View, StyleSheet, Platform } from 'react-native';
 import renderChart from './renderChart';
 import echarts from './echarts.min';
 import { WebView } from 'react-native-webview'
